ea85ceb2546d7cd65d3c244e39926967d234b97d
Lasse Schlör <lasse-schloer@servermx.de>
2023-06-29 15:46:47 +0200

    changed names of the subcommands

--- ifconn.sh
+++ ifconn.sh
@@ -12,28 +12,28 @@ usage_msg="\
 This is a shell script for easy Wi-Fi control intended for Raspberry Pis.
 
 Usage:
-  $0 start       [interface]
-  $0 stop        [interface]
-  $0 restart     [interface]
-  $0 autorestart [interface]
+  $0 connect     [interface]
+  $0 disconnect  [interface]
+  $0 reconnect   [interface]
+  $0 autoconnect [interface]
 
 …where \`interface\` is \`$interface\` by default.
 
-\`start\`/\`stop\` map to \`$ifconfig_command <interface> <up/down>\`, \
+\`connect\`/\`disconnect\` map to \`$ifconfig_command <interface> <up/down>\`, \
 respectively.
-\`restart\` issues a \`start\` and then a \`stop\`.
-\`autorestart\` issues a \`restart\` iff the local gateway is not reachable.
+\`reconnect\` issues a \`connect\` and then a \`disconnect\`.
+\`autoconnect\` issues a \`reconnect\` iff the local gateway is not reachable.
 
 The script makes use of \`$log_command\` for logging.
 
-As a particular example, the \`autorestart\` subcommand may help with issues \
+As a particular example, the \`autoconnect\` subcommand may help with issues \
 where
 the Pi looses the Wi-Fi connection and does not reconnect on its own. To
 automatically reconnect every 5 minutes and turn Wi-Fi off during the night, put
 the following in a root-privileged crontab:
 
-*/5 9-19 * * * ifconn autorestart
-  5   20 * * * ifconn stop
+*/5 9-19 * * * ifconn autoconnect
+  5   20 * * * ifconn disconnect
 
 …and make sure to replace \`ifconn\` by a path to this script if it is not
 available on the \`\$PATH\`."
@@ -44,27 +44,24 @@ case $# in
   *) printf "%s\n" "$usage_msg" >&2 ; exit 1 ;;
 esac
 
-# Use the past verb without "ed"-prefix for subcommands internally
-[ "$subcommand" = "stop" ] && subcommand="stopp"
-# …for easy message creation
 log_msg="connection on $interface ${subcommand}ed via $0"
 
 case "$subcommand" in
-  "start")
+  "connect")
     eval "$ifconfig_command" "$interface" up
     eval "$log_command" "\"$log_msg\""
     ;;
-  "stopp")
+  "disconnect")
     eval "$ifconfig_command" "$interface" down
     eval "$log_command" "\"$log_msg\""
     ;;
-  "restart")
+  "reconnect")
     eval "$ifconfig_command" "$interface" down
     eval "$ifconfig_command" "$interface" up
     eval "$log_command" "\"$log_msg\""
     ;;
-  "autorestart")
-    # Find default gateway, ping it, and restart interface if no response
+  "autoconnect")
+    # Find default gateway, ping it, and reconnect interface if no response
     # Adapted in part from:
     # `https://forums.raspberrypi.com/viewtopic.php?t=239730`
     gateway=$(eval "$ip_command" -4 route show default dev "$interface" | \
